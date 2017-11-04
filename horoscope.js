da52bbb0ca5d659324e55c36bcb28f59e28372bd
phildierks <phillipdierks@students.berkeley.net>
2017-11-03 23:21:57 -0700

    Horoscope Project

--- horoscope.js
+++ horoscope.js
@@ -1,36 +1,59 @@
 function start(){
-    var name = document.getElementById("name").value;
-    var month = document.getElementById("month").value;
-    var day = document.getElementById("day").value;
-    var sign = determineHoroscope(month,day,name).value;
-    console.log(month + day + name);
+    onSubmit();
+    console.log(name + month + day);
+    document.getElementById("horoscope").innerHTML = determineHoroscope();
 }
 
-
-function determineHoroscope(month,day, name){
-    if(month == 1 &&  day >= 20 || (month == 2 && day <=18)){
-        return name + ", You are an Aquarius";
-    }else if(month == 2 &&  day>= 19 ||(month == 3 && day<=20)){
-        return name + ", You are a Pisces";
-    }else if(month == 3 && day>= 21 || (month == 4 && day<= 19)){
-        return name + ", You are an Aries";
-    }else if(month == 4 && day >= 20 || (month == 5 && day<= 20)){
-        return name + ", You are a Taurus";
-    }else if(month == 5 && day >= 21 || (month == 6 && day <= 20)){
-        return name + ", You are a Gemini";
-    }else if(month == 6 && day >= 21 || (month == 7 && day <= 22)){
-        return name + ", You are a Cancer";
-    }else if(month == 7 && day>= 23 || (month == 8 && day <= 22)){
-        return  name + ", You are a Leo";
-    }else if(month == 8 && day >= 23 || (month == 9 && day <= 22)){
-        return name + ", You are a Virgo";
-    }else if(month == 9 && day >= 23 || (month == 10 && day<= 22)){
-        return name + ", You are a Libra";
-    }else if(month == 10 && day >=23 || (month ==  11 && day <=21)){
-        return name +  ", You are a Scorpio";
-    }else if(month == 11 && day >=22 || (month == 12 && day <= 21)){
-        return name + ", You are a Sagittarius";
-    }else{
-        return name + ", You are a Capricorn";
+function onSubmit(){
+    name = document.getElementById("name").value;
+    month = document.getElementById("month").value;
+    day = document.getElementById("days").value;
+}
+function yearBox() {
+    var result ="";
+    for(var i = 1; i <= 100; i++){
+        result+="<option value =" + i + ">" + (i+ 1899) + "</option>"
     }
+    document.getElementById("years").innerHTML=result;
+
+}
+function doDays(month){
+    var mDays = [0,31,28,31,30,31,30,31,31,30,31,30,31];
+    var result = "";
+    for(var i = 1; i <= mDays[month]; i++){
+        result += "<option value =" + i + ">" + i + "</option>"
+    }
+    document.getElementById("days").innerHTML=result;
+}
+
+var sign ="";
+function determineSign(month,day){
+    if(month == 1 && day >= 20 || (month == 2 && day <=18)){
+        sign += "Aquarius";
+    }else if(month == 2 && day>= 19 ||(month == 3 && day<=20)){
+        sign += "Pisces";
+    }else if(month == 3 && day>= 21 || (month == 4 && day<= 19)){
+        sign += "Aries";
+    }else if(month == 4 && day >= 20 || (month == 5 && day<= 20)){
+        sign += "Taurus";
+    }else if(month == 5 && day >= 21 || (month == 6 && day <= 20)){
+        sign += "Gemini";
+    }else if(month == 6 && day >= 21 || (month == 7 && day <= 22)){
+        sign += "Cancer";
+    }else if(month == 7 && day>= 23 || (month == 8 && day <= 22)){
+        sign += "Leo";
+    }else if(month == 8 && day >= 23 || (month == 9 && day <= 22)){
+        sign += "Virgo"
+    }else if(month == 9 && day >= 23 || (month == 10 && day<= 22)){
+        sign+= "Libra";
+    }else if(month == 10 && day >=23 || (month ==  11 && day <=21)){
+        sign += "Scorpio";
+    }else if(month == 11 && day >=22 || (month == 12 && day <= 21)){
+        sign+= "Sagittarius";
+    }else{
+        sign += "Capricorn";
+    }
+}
+function determineHoroscope(){
+    return name + ", you are a " + sign;
 }
